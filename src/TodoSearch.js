cd841b66f6b0eaeb02afc3b119e1cc33d9e66c52
Angie Cabanillas <acabanillasb2@upao.edu.pe>
2022-10-10 07:49:00 -0500

    chore: Estado

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,17 +2,23 @@ import React from 'react';
 import './TodoSearch.css'
 
 function TodoSearch() {
+    const [search,setSearch] = React.useState('');
+  
     const onSearchChange = (event)=> {
       console.log(event.target.value);
+      //cambia el estado
+      setSearch(event.target.value);
     }
 
-    return(
+    return[
       <input 
         className='search' 
         placeholder="Enter the TODO to search"
+        value={search}
         onChange={onSearchChange}
-      />
-    );
+      />,
+      <p>{search}</p>
+    ];
 }
 
 export {TodoSearch};
